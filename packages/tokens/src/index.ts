a66d27d5b983ce3f15bb4df68c214aa07a4b9f53
William Santana <euwilliamsantana@gmail.com>
2024-05-03 16:24:03 -0300

    Init - Monorepo + ESLint + TSConfig

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+export * from './colors'
+
+export * from './font-sizes'
+export * from './font-weights'
+export * from './fonts'
+export * from './line-height'
+
+export * from './radii'
+export * from './space'
