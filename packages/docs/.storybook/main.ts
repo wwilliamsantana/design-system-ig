f299ceb570e5963afbe053b9b15b4262a5cdf721
William Santana <euwilliamsantana@gmail.com>
2024-06-17 14:44:49 -0300

    feat: Pages + Component Color

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  "stories": ["../src/stories/**/*.mdx", "../src/stories/**/*.stories.tsx"],
+  "stories": ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
 
   "addons": [
     "@storybook/addon-links",
